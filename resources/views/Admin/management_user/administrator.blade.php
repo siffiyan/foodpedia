a0a908e7c4fc38e5926cb98fe17df539137ece40
armadhilo <dunsforce@gmail.com>
2020-08-10 22:48:05 +0700

    front end administrator

--- resources/views/Admin/management_user/administrator.blade.php
+++ resources/views/Admin/management_user/administrator.blade.php
@@ -1,46 +1,204 @@
 @extends('partials.app')
 
-@section('title','Dashboard')
+@section('title','Administrator')
 
 @section('content')
 
 <div class="row">
     <div class="col-12">
-      <div class="card">
+        <button class="btn btn-primary mb-3" id="btn_modal_add"><i class="fa fa-plus-square"></i> &nbsp; Add User</button>
+        <div class="card">
         <div class="card-header">
-          <h3 class="card-title">DataTable with default features</h3>
+            <h3 class="card-title">DataTable with default features</h3>
         </div>
         <!-- /.card-header -->
         <div class="card-body">
-          <table id="example1" class="table table-bordered table-striped">
+            <table id="example1" class="table table-bordered table-striped">
             <thead>
             <tr>
-              <th>Rendering engine</th>
-              <th>Browser</th>
-              <th>Platform(s)</th>
-              <th>Engine version</th>
-              <th>CSS grade</th>
+                <th>Username</th>
+                <th>Pasword</th>
+                <th class="text-center">Status</th>
+                <th class="text-center">User Level</th>
+                <th class="text-center">Action</th>
             </tr>
             </thead>
             <tbody>
             <tr>
-              <td>Trident</td>
-              <td>Internet
-                Explorer 4.0
-              </td>
-              <td>Win 95+</td>
-              <td> 4</td>
-              <td>X</td>
+                <td>Admin</td>
+                <td>Password</td>
+                <td class="text-center">
+                    <span class="badge badge-success" onclick="inactive()" style="cursor:pointer">active</span>
+                </td>
+                <td class="text-center">Admin</td>
+                <td class="text-center">
+                    <button class="btn btn-info" onclick="edit()"><i class="fa fa-edit"></i></button>
+                    <button class="btn btn-danger" onclick="hapus()"><i class="fa fa-trash"></i></button>
+                </td>
             </tr>
             </tbody>
-          </table>
+            </table>
         </div>
         <!-- /.card-body -->
-      </div>
-      <!-- /.card -->
+        </div>
+        <!-- /.card -->
     </div>
     <!-- /.col -->
-  </div>
+</div>
+
+<div class="modal fade" id="modal_add">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Modal Add User</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <label>USER LEVEL</label>
+                        <select class="form-control" name="id_user_level" id="user_level_list"></select>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <label>NAMA</label>
+                        <input type="text" class="form-control" name="nama_user" id="nama_user"></input>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="form-group">
+                        <label>PASSWORD</label>
+                        <input type="password" class="form-control" name="password_user" id="password_user"></input>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <label>STATUS</label>
+                        <select class="form-control" name="isactive">
+                            <option value="1">ACTIVE</option>
+                            <option value="0">INACTIVE</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+<div class="modal fade" id="modal_edit">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Modal Add User</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <label>USER LEVEL</label>
+                        <select class="form-control" name="id_user_level" id="user_level_list"></select>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <label>NAMA</label>
+                        <input type="text" class="form-control" name="nama_user" id="nama_user"></input>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="form-group">
+                        <label>PASSWORD</label>
+                        <input type="password" class="form-control" name="password_user" id="password_user"></input>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        <label>STATUS</label>
+                        <select class="form-control" name="isactive">
+                            <option value="1">ACTIVE</option>
+                            <option value="0">INACTIVE</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+<div class="modal fade" id="delete_modal" role="dialog" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content text-center">
+            <div class="modal-body">
+                <div class="form-content p-2">
+                    <h4 class="modal-title">Delete</h4>
+                    <input type="hidden" name="id" id="id_delete">
+                    <p class="mb-4">Are you sure want to delete?</p>
+                    <button type="submit" class="btn btn-primary">Delete </button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="inactive_modal" role="dialog" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content modal-content bg-warning text-center">
+            <div class="modal-body">
+                <div class="form-content p-2">
+                    <h4 class="modal-title">Inactive</h4>
+                    <input type="hidden" name="id" id="id_delete">
+                    <p class="mb-4">Are you sure want to inactive?</p>
+                    <button type="submit" class="btn btn-outline-light">INACTIVE</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="active_modal" role="dialog" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content bg-success text-center">
+            <div class="modal-body">
+                <div class="form-content p-2">
+                    <h4 class="modal-title">Acivated</h4>
+                    <input type="hidden" name="id" id="id_delete">
+                    <p class="mb-4">Are you sure want to activatdd?</p>
+                    <button type="submit" class="btn btn-outline-light">ACTIVATED</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
 
@@ -52,6 +210,25 @@
         "autoWidth": false,
         });
     });   
+
+    $('#btn_modal_add').click(function(){
+        $('#modal_add').modal('show');
+    });
+
+    function edit(){
+        $('#modal_edit').modal('show');
+    }
+
+    function hapus(){
+        $('#delete_modal').modal('show');
+    }
+
+    function inactive(){
+        $('#inactive_modal').modal('show');
+    }
+       
+    
+
 </script>    
 @endsection
 
--- resources/views/partials/app.blade.php
+++ resources/views/partials/app.blade.php
@@ -11,6 +11,10 @@
   <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.css') }}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="{{asset('template/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
+  <!-- Toastr -->
+  <link rel="stylesheet" href="{{asset('template/plugins/toastr/toastr.min.css')}}">
   <!-- DataTables -->
   <link rel="stylesheet" href="{{asset('template/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
   <link rel="stylesheet" href="{{asset('template/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
@@ -72,6 +76,10 @@
 <script src=" {{ asset('template/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
 <script src=" {{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }} "></script>
+<!-- SweetAlert2 -->
+<script src="{{asset('template/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
+<!-- Toastr -->
+<script src="{{asset('template/plugins/toastr/toastr.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src=" {{ asset('template/dist/js/adminlte.min.js') }} "></script>
 <!-- AdminLTE for demo purposes -->
@@ -92,6 +100,21 @@
 
 <!-- PAGE SCRIPTS -->
 <script src="{{asset('template/dist/js/pages/dashboard2.js')}}"></script>
+
+<script>
+  $(function() {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000
+    });
+  });
+
+  $('document').ready(function(){
+    bsCustomFileInput.init();
+  })
+</script>
 
 @yield('js')
 
