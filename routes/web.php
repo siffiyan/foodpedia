633a3a7659687e58ad79b10373c6786c998cadc8
armadhilo <dunsforce@gmail.com>
2020-08-20 19:31:28 +0700

    make route better

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,7 @@ Route::prefix('admin')->group(function () {
     Route::get('/dashboard', 'SuperAdmin\DashboardController@index');
     Route::get('/management_user', 'SuperAdmin\managementUserController@index');
     Route::post('/management_user/store', 'SuperAdmin\managementUserController@store');
-
-    // Route::get('/management_project','SuperAdmin\ProjectController@index');
-    // Route::get('/management_project/create','SuperAdmin\ProjectController@create');
-    // Route::post('/management_project','SuperAdmin\ProjectController@store');
-
-    // Route::resource('termin','SuperAdmin\TerminController');
-
-    //Route::view('/management_project/pnpp_vendor','admin.management_project.pnpp_vendor');
-    // Route::resource('penpp_vendor', 'SuperAdmin\PenppVendorController');
-
+    
     Route::get('/management_master/vendor','SuperAdmin\VendorController@index');
     Route::post('/management_master/vendor','SuperAdmin\VendorController@store');
     Route::get('/management_master/vendor/{id}/edit','SuperAdmin\VendorController@edit');
